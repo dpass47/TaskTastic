a0ef60a50a364be2c0d3a47da0162310668c9575
Dante <dante@devdante.com>
2025-01-13 22:08:00 -0700

    Update Navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,27 +1,41 @@
 <script setup>
+import { ref } from 'vue';
 import { RouterLink, RouterView } from 'vue-router';
 import HamburgerIcon from './icons/HamburgerIcon.vue';
 
-function buttonClick() {
-	console.log('button clicked');
-}
+const navbarToggle = ref(false);
 </script>
 
 <template>
 	<div class="navbar-container">
-		<div class="navbar-menu">
-			<button @click="buttonClick">
+		<div
+			class="navbar-menu"
+			v-show="!navbarToggle">
+			<button @click="navbarToggle = !navbarToggle">
 				<HamburgerIcon />
 			</button>
-			<nav>
-				<RouterLink to="/">Home</RouterLink>
-				<RouterLink to="/settings">Settings</RouterLink>
-			</nav>
 		</div>
 		<h1>TaskTastic</h1>
-
 		<RouterView />
 	</div>
+	<nav v-show="navbarToggle">
+		<div class="navbar-header">
+			<h1>TaskTastic</h1>
+			<button @click="navbarToggle = !navbarToggle">X</button>
+		</div>
+		<div class="navbar-links-container">
+			<RouterLink
+				to="/"
+				class="navbar-link"
+				>Home</RouterLink
+			>
+			<RouterLink
+				to="/settings"
+				class="navbar-link"
+				>Settings</RouterLink
+			>
+		</div>
+	</nav>
 </template>
 
 <style scoped>
@@ -34,6 +48,7 @@ function buttonClick() {
 	display: flex;
 	place-items: center;
 	flex-direction: row-reverse;
+	border: 2px solid magenta;
 }
 
 button {
@@ -44,20 +59,53 @@ button {
 	height: 32px;
 	color: var(--color-accent);
 	background: none;
-	border: none;
+	border: 1px solid var(--color-accent);
+	border-radius: 0.5em;
+	transition: all ease 500ms;
+}
+
+button:hover {
+	cursor: pointer;
+	box-shadow: inset 100px 0 0 0 #272727;
 }
 
 h1 {
 	margin-left: 0.25em;
 }
 
-button:hover {
-	cursor: pointer;
-}
-
 nav {
 	display: flex;
 	flex-direction: column;
-	display: none;
+	/* justify-content: space-between; */
+	margin-right: 2em;
+	position: absolute;
+	background-color: #272727;
+	top: 0px;
+	left: 0px;
+	padding: 1em;
+	width: 300px;
+	border-bottom-right-radius: 0.5em;
+}
+
+nav h1 {
+	margin: 0;
+	padding: 0;
+}
+
+nav button:hover {
+	color: white;
+}
+
+.navbar-links-container {
+	display: flex;
+	flex-direction: column;
+	/* border: 2px solid magenta; */
+	width: 100%;
+}
+
+.navbar-header {
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
 }
 </style>
